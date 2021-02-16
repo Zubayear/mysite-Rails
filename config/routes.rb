f7b2e7786140e0d18306f99dc606411bd78c2d41
Zubayear <syedjubaer997@gmail.com>
2021-02-16 16:30:25 +0600

    Route configure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
-  resources :articles
-  resources :projects
+  resources :articles, only: [:index, :show]
+  resources :projects, only: [:index, :show]
 end
