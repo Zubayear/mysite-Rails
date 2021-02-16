07e27499d87d33386e760057ddd07181caadedb5
Zubayear <syedjubaer997@gmail.com>
2021-02-16 14:26:43 +0600

    Update the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
-  resources :articles, only: [:index, :show]
+  resources :articles
   resources :projects
 end
